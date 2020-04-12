418520a8730b13c8a1d33c561f9d4f24b6c19b3b
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-12 18:32:13 +0300

    If Two Sixs Lose Scores

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
 let scores, roundScore, activePlayer, gamePlaying;
 
 init();
+
+let lastDice;
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
 	if (gamePlaying) {
@@ -24,7 +15,12 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 		diceDOM.src = `dice-${dice}.png`;
 
 		// update round score acc. to player if dice !== 0
-		if (dice !== 1) {
+		if (dice === 6 && lastDice === 6) {
+			// player loses score
+			scores[activePlayer] = 0;
+			document.querySelector(`#score-${activePlayer}`).textContent = '0';
+			nextPlayer();
+		} else if (dice !== 1) {
 			// add score
 			roundScore += dice;
 			document.querySelector(
@@ -34,6 +30,8 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 			// next player
 			nextPlayer();
 		}
+
+		lastDice = dice;
 	}
 });
 
@@ -47,7 +45,7 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 			scores[activePlayer];
 
 		// check if player won the game
-		if (scores[activePlayer] >= 20) {
+		if (scores[activePlayer] >= 100) {
 			document.querySelector(`#name-${activePlayer}`).textContent = 'WINNER!';
 			document.querySelector('.dice').style.display = 'none';
 			document
