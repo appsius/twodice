fa665f3d919e2b1ac5f1fa350040528246389098
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-12 13:55:00 +0300

    Events & Event Handling / Roll

--- app.js
+++ app.js
@@ -8,13 +8,26 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer, dice;
+
+let scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+document.querySelector('.dice').style.display = 'none';
 
-document.querySelector(`#current-${activePlayer}`).textContent = dice;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+	// random num
+	const dice = Math.floor(Math.random() * 6) + 1;
+
+	// display result
+	let diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = `dice-${dice}.png`;
+});
